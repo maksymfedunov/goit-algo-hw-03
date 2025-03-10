0c2c049d11df5148eb4fdf3e98228c07631ebb59
maksymfedunov <maksim.kherson88@gmail.com>
2025-03-10 20:48:10 +0100

    second commit

--- home_work_modul_3_2.py
+++ home_work_modul_3_2.py
@@ -3,9 +3,11 @@ import random
 
 def get_numbers_ticket(min, max, quantity):
     try:
-        if min >= 1 and max <= 1000 and min <= quantity <= max: # задать диапазон для аргументов
+        if min >= 1 and max <= 1000 and 1 <= quantity <= (max - min +1): # задать диапазон для аргументов
             lottery_numbers = list(sorted(set(random.sample(range(min, max+1), quantity)))) # Сгенерировать отсортированный набор уникальных чисел
-            print("Выигрышные лотерейные числа:", lottery_numbers)
+            print(lottery_numbers)
+            return lottery_numbers
+            
         else:                                
             print("Неверно введены данные")
             return []
@@ -13,4 +15,4 @@ def get_numbers_ticket(min, max, quantity):
         print("Введите пожалуйста данные в цифрах") 
         return []        
         
-get_numbers_ticket(1, 36, 6)
+
